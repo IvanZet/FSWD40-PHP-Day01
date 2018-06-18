bb9029c4a235b06a5a437ef17949746bf8ddeac6
Ivan Zykov <ivan.v.zykov@gmail.com>
2018-06-18 09:27:00 +0200

    Adds files

---
+++ Exercises_PHP_Day1.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<!--Exercise 1
+	Print your name in an h1 HTML element using PHP (define the variable and store your name into it). Use external CSS to position your name in the middle of the screen (horizontal, vertical).-->
+	
+	<?php
+	$myName = 'Ivan';
+	?>
+
+	<h1><?php echo $myName ?></h1>
+
+</body>
+</html>
