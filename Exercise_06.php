64ef10dabbf398e93fca73e255a80defb2fea5f3
Denis Schuele <schueledenis@gmx.at>
2018-06-18 10:49:56 +0200

    ex 6

--- Exercise_06.php
+++ Exercise_06.php
@@ -8,9 +8,27 @@
 </head>
 <body>
     <h1>Exercise 6</h1>
+    <h3>first Version</h3 >
     <?php 
-
-
+        $names = [
+            "Mickey" => "Maus",
+            "Spider" => "man",
+            "The huge" => "Hulg",
+        ];
+        foreach($names as $index => $value){
+            echo $index." ".$value.".<br>";
+        }
+        ?>
+        <h3>second Version</h3 >
+        <?php 
+        $namesb = [
+            "Mickey" => "Mickey Maus",
+            "Spider" => "Spiderman",
+            "green" => "The huge Hulg",
+        ];
+        foreach($namesb as $index => $value){
+            echo $index." ".$value.".<br>";
+        }
     ?>
 
 </body>
