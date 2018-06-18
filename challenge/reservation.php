752ce71321ab3bb2aea4fa7d3e4a98a790c3c03f
Denis Schuele <schueledenis@gmx.at>
2018-06-18 16:00:35 +0200

    reservation site

--- challenge/reservation.php
+++ challenge/reservation.php
@@ -1,12 +1,17 @@
+<?php include("data.php");?>
+
 <!doctype html>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Cool wheels</title>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/gijgo@1.9.6/js/gijgo.min.js" type="text/javascript"></script>
+    <link href="https://cdn.jsdelivr.net/npm/gijgo@1.9.6/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+	
 </head>
 <body>
 	<header>
@@ -32,31 +37,48 @@
 	</header>
 	<main>
 		<h1>Fill the form to book a car</h1>
-
-		<form>
+		<div class="row">
+		<div class="col-8 col-md-offset-2">
+		<form >
 
 			<select class="custom-select">
-			  <option selected>Open this select menu</option>
-			  <option value="1">One</option>
+				<option selected>Select your car</option>
+				<?php 
+				$i = 1;
+				foreach($fleet as $car => $features){
+					?>
+					<option value="<?php echo $i++ ?>"><?php echo $car?></option>
+					<?php
+				}	
+				?>
 			</select>
 
-		  <div class="form-group">
-		    <label for="exampleFormControlInput1">Email address</label>
-		    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-		  </div>
-		  <div class="form-group">
-		    <label for="exampleFormControlInput1">Email address</label>
-		    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-		  </div>
+			<div class="form-group">
+				<label for="exampleFormControlInput1">First name</label>
+				<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Max">
+			</div>
+			<div class="form-group">
+				<label for="exampleFormControlInput1">Second name</label>
+				<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Musterman">
+			</div>
+			<div class="input-group date" data-provide="datepicker">
+    <input type="text" class="form-control">
+    <div class="input-group-addon">
+        <span class="glyphicon glyphicon-th"></span>
+    </div>
+</div>
+	<input id="datepicker">
 
-		  
 
-		  <div class="form-group">
-		    <label for="exampleFormControlTextarea1">Example textarea</label>
-		    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-		  </div>
+			<div class="form-group">
+				<label for="exampleFormControlTextarea1">Example textarea</label>
+				<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+			</div>
 		</form>
-
+		</div>		
+		
+		</div>
 	</main>
+	<script> $('#datepicker').datepicker();</script>
 </body>
 </html>
