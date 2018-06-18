1338e268657e7e5fe6c65357a64ed2803d4acf6c
Denis Schuele <schueledenis@gmx.at>
2018-06-18 09:57:53 +0200

    exercise 2

--- Exercise_02.php
+++ Exercise_02.php
@@ -1,1 +1,24 @@
-htm
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $msg;
+    $targetDay = "Mon";
+    $actualDay = date("D");
+    if($actualDay == $targetDay){
+        $msg = "Happy Monday";
+    } else{
+        $msg = "Have a nice day!";
+    }
+
+    ?>
+    <h1><?php echo $msg ?></h1>
+
+</body>
+</html>
